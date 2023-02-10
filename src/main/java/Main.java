1125fff33a73341e8c4d9930f9415728e2e2df42
Martyn022 <mg.sharipov@gmail.com>
2023-02-10 19:43:52 +0500

    Вводная в Гит, вторая.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
-//import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
 
         SalesManager salesManager1 = new SalesManager(new int[9]);
-
-        //int[] sale = {7, 10, 3};
 
         System.out.println(salesManager1.max());
 
