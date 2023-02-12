998a7ea7156c661ba42099f979d19b46eee0ef82
Martyn022 <mg.sharipov@gmail.com>
2023-02-12 20:53:28 +0500

    Отвод ветки longer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        SalesManager salesManager1 = new SalesManager(new int[9]);
+        SalesManager salesManager1 = new SalesManager(new int[9];
 
         System.out.println(salesManager1.max());
 
